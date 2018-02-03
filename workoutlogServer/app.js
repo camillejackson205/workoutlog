d162b4aba74027af796d9c5ca03e5b0aba4b3b25
Camille Jackson <missjackson2626@yahoo.com>
2018-02-03 10:59:58 -0500

    commiting mod7

--- workoutlogServer/app.js
+++ workoutlogServer/app.js
@@ -1,6 +1,32 @@
-express = require('express');
+var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+
+app.use(require('./middleware/headers'));
+
+app.use('/api/test', function(req, res){
+    res.send("Hello World");
+});
+var Sequelize = require ('sequelize');
+var sequelize = new Sequelize('awesomeworkoutlog', 'postgres', 'skip983825', {
+    host: 'localhost',
+    dialect: 'postgres'
+});
+sequelize.authenticate().then(
+    function() {
+        console.log('connected to workoutlog postgres db');
+    },
+    function(err){
+        console.log(err);
+    }
+);
+var User = sequelize.define('user', {
+    username: Sequelize.STRING,
+    passwordhash: Sequelize.STRING
+});
+User.sync();
+//User.sync({force:true});
+app.use(bodyParser.json());
 
 app.post('/api/user', function(req, res){
     var username = req.body.user.username;
@@ -12,45 +38,23 @@ app.post('/api/user', function(req, res){
         //sequelize is going to return the object it created from the db.
 
         function createSuccess(user){
-
+            res.json({
+                user:user,
+                message: 'create'
+            });
         },
         function createError(err){
-
+            res.send(500, err.message);
         }
     );
     //need to create a user object and use sequelize to put that user into our db.
 });
 
-var Sequelize = require ('sequelize');
-var sequelize = new Sequelize('workoutlog', 'postgres', 'skip983825', {
-    host: 'localhost',
-    dialect: 'postgres'
-});
 
-sequelize.authenticate().then(
-    function() {
-        console.log('connected to workoutlog postgres db');
-    },
-    function(err){
-        console.log(err);
-    }
-);
 
-app.use(require('./middleware/headers'));
-
-app.use('/api/test', function(req, res){
-    res.send("Hello World");
-});
 
 app.listen(3000, function(){
     console.log("app is listening on 3000");
 });
 
-var User = sequelize.define('user', {
-    username: Sequelize.STRING,
-    passwordhash: Sequelize.STRING
-});
 
-User.sync();
-//User.sync({force:true});
-app.use(bodyParser.json());
