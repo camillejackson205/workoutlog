7b0127b9cb3a4dfa12c47eb3de81e96dfbf05b34
Camille Jackson <missjackson2626@yahoo.com>
2018-02-03 10:29:51 -0500

    commiting mod 7

--- workoutlogServer/app.js
+++ workoutlogServer/app.js
@@ -1,5 +1,40 @@
 express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+app.post('/api/user', function(req, res){
+    var username = req.body.user.username;
+    var pass = req.body.user.password;
+    User.create({
+        username: username,
+        passwordhash: ""
+    }).then(
+        //sequelize is going to return the object it created from the db.
+
+        function createSuccess(user){
+
+        },
+        function createError(err){
+
+        }
+    );
+    //need to create a user object and use sequelize to put that user into our db.
+});
+
+var Sequelize = require ('sequelize');
+var sequelize = new Sequelize('workoutlog', 'postgres', 'skip983825', {
+    host: 'localhost',
+    dialect: 'postgres'
+});
+
+sequelize.authenticate().then(
+    function() {
+        console.log('connected to workoutlog postgres db');
+    },
+    function(err){
+        console.log(err);
+    }
+);
 
 app.use(require('./middleware/headers'));
 
@@ -10,3 +45,12 @@ app.use('/api/test', function(req, res){
 app.listen(3000, function(){
     console.log("app is listening on 3000");
 });
+
+var User = sequelize.define('user', {
+    username: Sequelize.STRING,
+    passwordhash: Sequelize.STRING
+});
+
+User.sync();
+//User.sync({force:true});
+app.use(bodyParser.json());
