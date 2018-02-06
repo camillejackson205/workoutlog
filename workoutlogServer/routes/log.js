a3902e24bdc38c2a050db093a07cd74a8ebb9aff
Camille Jackson <missjackson2626@yahoo.com>
2018-02-05 23:29:00 -0500

    module22 finished

--- workoutlogServer/routes/log.js
+++ workoutlogServer/routes/log.js
@@ -1,49 +1,65 @@
 var router = require('express').Router();
 var sequelize = require('../db');
-var log = sequelize.import('../models/log');
+var Log = sequelize.import('../models/log');
 var User = sequelize.import('../models/user');
 var Definition = sequelize.import('../models/definition');
 
-router.post('/', function(req, res){
-    //req has some body properties that have a username and pwd
-    var description = req.body.log.desc;
-    var result = req.body.log.result;
+router.post('/', function(req, res) {
+    // req has some body properties that have a username and pwd
+    var description = req.body.log.description; 
+    var result = req.body.log.result; 
     var user = req.user;
     var definition = req.body.log.def;
+   
 
-    //Use our sequelize model to create log
-    Log
-        .create({
-            description: description,
-            result: result,
-            owner: user.id,
-            def: definition
-        })
-        .then(
-            function createSuccess(log) {
-                res.json(log);
-            },
-            function createError(err) {
-                res.send(500, err.message);
-            }
-        );
+    // Use our sequelize model to create log
+  	Log 
+	    .create({ 
+	    	description: description,
+	    	result: result,
+	    	owner: user.id,
+	    	def: definition
+	    })
+	    .then(
+	    	function createSuccess(log) {
+	    		res.json(log);
+	    	}, 
+		    function createError(err) {
+		        res.send(500, err.message);
+		    }
+	    );
 });
 
-router.get('/', function(req, res){
-    var userid = req.user.id;
-    Log
-    .findAll({
-        where: { owner: userid }
-    })
-    .then(
-        function findAllSuccess(data) {
-            //console.log(data);
-            res.json(data);
-        },
-        function findAllError(err) {
-            res.send(500, err.message);
-        }
-    );
+router.get('/', function(req, res) {
+	var userid = req.user.id;
+	Log
+	.findAll({
+		where: { owner: userid }
+	})
+	.then(
+		function findAllSuccess(data) {
+			// console.log(data);
+			res.json(data);
+		},
+		function findAllError(err) {
+			res.send(500, err.message);
+		}
+	);
+});
+
+router.delete('/', function(req, res) {
+	var data = req.body.log.id;
+	Log
+		.destroy({
+			where: { id: data }
+		}).then(
+			function deleteLogSuccess(data){
+				res.send("you removed a log");
+			},
+			function deleteLogError(err){
+				res.send(500, err.message);
+			}
+		);
 });
 
 module.exports = router;
